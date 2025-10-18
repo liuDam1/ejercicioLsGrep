72dc0cd05c62cf83eebc55f74b1035c190420c8e
liuDam1 <liu.jiangyu.dam@gmail.com>
2025-10-17 21:50:50 -0500

    Sin Dublicar Codigo

--- src/main/java/es/etg/psp/lsgrep/Main.java
+++ src/main/java/es/etg/psp/lsgrep/Main.java
@@ -8,68 +8,49 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 public class Main {
-
     public static final String MSG_ERROR = "Se ha producido un error al ejecutar el comando";
     public static final String[] COMANDO_LS = { "ls" };
     public static final String[] COMANDO_GREP = { "grep", "a" };
-    public static final String ENCABEZADO = "Archivos que contienen > " + COMANDO_GREP[1];
-    public static final String SEPARADOR = "----------------------------";
-    public static final String TERMINO_LS = "LS terminó con código: ";
-    public static final String TERMINO_GREP = "GREP terminó con código: ";
+    public static final String SALTO_LINEA = "\n";
 
     public static void main(String[] args) {
-        Process procesoLS = null;
-        Process procesoGrep = null;
-
         try {
-            procesoLS = Runtime.getRuntime().exec(COMANDO_LS);
-            procesoGrep = Runtime.getRuntime().exec(COMANDO_GREP);
-
-            BufferedReader salidaLS = new BufferedReader(
-                    new InputStreamReader(procesoLS.getInputStream()));
-
-            OutputStream entradaGrep = procesoGrep.getOutputStream();
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(entradaGrep));
-
-            String linea;
-            while ((linea = salidaLS.readLine()) != null) {
-                writer.println(linea);
-            }
-
-            writer.flush();
-            writer.close();
-            salidaLS.close();
-
-            BufferedReader salidaGrep = new BufferedReader(
-                    new InputStreamReader(procesoGrep.getInputStream()));
-
-            System.out.println(ENCABEZADO);
-            System.out.println(SEPARADOR);
-
-            while ((linea = salidaGrep.readLine()) != null) {
-                System.out.println(linea);
-            }
-
-            salidaGrep.close();
-
-            int exitValLS = procesoLS.waitFor();
-            int exitValGrep = procesoGrep.waitFor();
-
-            if (exitValLS == 0 && exitValGrep == 0) {
-                System.exit(0);
-            } else {
-                System.out.println(MSG_ERROR);
-                System.exit(1);
-            }
-
-        } catch (IOException | InterruptedException e) {
+            String salidaLs = ejecutarComando(COMANDO_LS);
+            String salidaGrep = ejecutarComando(COMANDO_GREP, salidaLs);
+            System.out.println(salidaGrep);
+            System.exit(0);
+        } catch (IOException e) {
             System.out.println(MSG_ERROR);
-            System.exit(1);
-        } finally {
-            if (procesoLS != null)
-                procesoLS.destroy();
-            if (procesoGrep != null)
-                procesoGrep.destroy();
+            System.exit(34);
         }
+    }
+
+    private static String leerSalida(Process proceso) throws IOException {
+        StringBuilder resultado = new StringBuilder();
+        try (BufferedReader lector = new BufferedReader(new InputStreamReader(proceso.getInputStream()))) {
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                resultado.append(linea).append(SALTO_LINEA);
+            }
+        }
+        return resultado.toString();
+    }
+
+    private static String ejecutarComando(String[] comando, String entrada) throws IOException {
+        Process proceso = Runtime.getRuntime().exec(comando);
+
+        if (entrada != null) {
+            try (OutputStream output = proceso.getOutputStream();
+                    PrintWriter pw = new PrintWriter(new OutputStreamWriter(output))) {
+                pw.print(entrada);
+                pw.flush();
+            }
+        }
+
+        return leerSalida(proceso);
+    }
+
+    private static String ejecutarComando(String[] comando) throws IOException {
+        return ejecutarComando(comando, null);
     }
 }
